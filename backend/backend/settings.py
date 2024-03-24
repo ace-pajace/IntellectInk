1c0bb62c2861cb7fd4c1121a26ce078d4a3fcd16
pvtrov <aga.patro@gmail.com>
2024-03-24 23:55:02 +0100

    xd

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'intellectink',
-    'courses',
-    'authentication'
+    'backend.intellectink',
+    'backend.courses',
+    'backend.authentication',
 ]
 
 MIDDLEWARE = [
