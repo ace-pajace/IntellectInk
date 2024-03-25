33fdb2e8c603467f3218be51dd285622243ce445
pvtrov <aga.patro@gmail.com>
2024-03-25 22:08:33 +0100

    wyswietlanie dodano

--- backend/backend/courses/views.py
+++ backend/backend/courses/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from ..intellectink.models import CourseAccess, Courses, Users
 
-# Create your views here.
+
+@login_required
+def get_user_courses(request):
+    user_email = request.user.email
+    user_courses = CourseAccess.objects.filter(user__email=user_email).select_related('course')
+
+    courses_data = [{
+        'name': access.course.name,
+        'term': access.course.term,
+        'edition': access.course.edition,
+        'access_level': access.access_level
+    } for access in user_courses]
+
+    return JsonResponse({'courses': courses_data})
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s=*17i+feg&d73he1_&o#xu$@og34mujx&ke!!aa#dox(gu6wn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
