57c064999c7bee484765e6043b23855df1f85b14
piaccho <sebi77styp@gmail.com>
2024-03-25 14:01:45 +0100

    Add init routing and pages

---
+++ frontend/src/pages/NoMatchPage.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const NoMatchPage: React.FC = () => {
+    // const navigate = useNavigate();
+
+    // const handleGoBack = () => {
+    //     navigate("/");
+    // };
+
+    return (
+        <div></div>
+        // <Container 
+        //     maxWidth="lg"
+        //     component="main"
+        //     sx={{
+        //         display: 'flex',
+        //         flexDirection: 'column',
+        //         alignItems: 'center',
+        //         justifyContent: 'center',
+        //         marginTop: '5rem',
+        //     }}
+        //     >
+        //         <Typography variant="h4" component="h1" mb={3}>
+        //             404 - Page Not Found
+        //         </Typography>
+        //         <Typography variant="body1" mb={5}>
+        //             Oops! The page you are looking for does not exist.
+        //         </Typography>
+        //         <Button variant="contained" color="primary" onClick={handleGoBack}>
+        //             Go Back
+        //         </Button>
+        // </Container>
+    );
+};
+
+export default NoMatchPage;
