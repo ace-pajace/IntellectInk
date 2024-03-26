4358e489018c7e6d6b8454acfa91368ee499e0a3
piaccho <sebi77styp@gmail.com>
2024-03-26 21:22:45 +0100

    feat: Add init page, sign-in page, sign-up page and setup routing

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -1,70 +1,96 @@
-import * as React from 'react';
+'use client'
+
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from "@chakra-ui/react";
+import LoginNavbar from "../components/LoginNavbar";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SignInPage() {
-//   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     const data = new FormData(event.currentTarget);
-//     console.log({
-//       login: data.get('login'),
-//       password: data.get('password'),
-//     });
-//   };
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // const [loginResponse, setLoginResponse] = useState(null);
+
+  const handleSignIn = async () => {
+    // const response = await ApiService.login(email, password);
+    // setLoginResponse(response);
+    console.log(`Email: ${email}, Password: ${password}`);
+  }
 
   return (
-    <div></div>
-    //   <Container component="main" maxWidth="xs">
-    //       <Box
-    //           my={5}
-    //           sx={{
-    //               display: 'flex',
-    //               flexDirection: 'column',
-    //               alignItems: 'center',
-    //           }}
-    //       >
-    //           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-    //               <LockOutlinedIcon />
-    //           </Avatar>
-    //           <Typography component="h1" variant="h5">
-    //               Sign in
-    //           </Typography>
-    //           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-    //               <TextField
-    //                   margin="normal"
-    //                   required
-    //                   fullWidth
-    //                   id="login"
-    //                   label="Login"
-    //                   name="login"
-    //                   autoComplete="login"
-    //                   autoFocus
-    //               />
-    //               <TextField
-    //                   margin="normal"
-    //                   required
-    //                   fullWidth
-    //                   name="password"
-    //                   label="Password"
-    //                   type="password"
-    //                   id="password"
-    //                   autoComplete="current-password"
-    //               />
-    //               <Button
-    //                   type="submit"
-    //                   fullWidth
-    //                   variant="contained"
-    //                   sx={{ mt: 3, mb: 2 }}
-    //               >
-    //                   Sign In
-    //               </Button>
-    //               <Grid container>
-    //                   <Grid item>
-    //                       <Link component={RouterLink} to="/sign-up" variant="body2">
-    //                           Don't have an account? Sign Up
-    //                       </Link>
-    //                   </Grid>
-    //               </Grid>
-    //           </Box>
-    //       </Box>
-    //   </Container>
+    <Box>
+      <LoginNavbar />
+      <Flex
+        // minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}
+      >
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"}>Sign in</Heading>
+            <Text fontSize={"lg"} color={"gray.600"}>
+              Sign in to access the functionality
+            </Text>
+          </Stack>
+          <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.700")}
+            boxShadow={"lg"}
+            p={8}
+          >
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />   
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: "column", sm: "row" }}
+                  align={"start"}
+                  justify={"space-between"}
+                >
+                  <Checkbox>Remember me</Checkbox>
+                  <Text color={"teal.400"}>Forgot password?</Text>
+                </Stack>
+                <Button
+                  onClick={handleSignIn} type="submit" as={'a'}
+                  bg={"teal.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "teal.500",
+                  }}
+                >
+                  Sign in
+                </Button>
+              </Stack>
+              <Stack pt={6}>
+                <Text align={"center"}>
+                  You don't have an account? <Link color={"teal.400"} onClick={() => navigate('/sign-up')}>Sign up</Link>
+                </Text>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    </Box>
   );
 }
