35f721843ec6b4ef43ca6d2ea1c4f6bc377c06fb
matimarecki <mati.marecki@gmail.com>
2024-03-26 18:09:27 +0100

    Feat: Restructured project. Added Create, Edit, Delete operations for Courses

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .authentication.views import register, LoginView
-from .courses.views import get_user_courses
+from .courses.views import get_user_courses, delete_course, edit_course, create_course
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register, name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('user/courses/', get_user_courses, name='get_user_courses')
+    path('user/courses/', get_user_courses, name='get_user_courses'),
+    path('user/courses/delete', delete_course, name='delete_course'),
+    path('user/courses/edit', edit_course, name='edit_course'),
+    path('user/courses/create', create_course, name='create_course')
 ]
