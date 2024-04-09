bfb9f074fb35d89c5b9ebc16f4e3c509badacfd5
s0jer <ppjaskowiec@gmail.com>
2024-04-10 01:17:29 +0200

    fix: fix docker-compose build

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,20 +1,19 @@
 "use client";
 
 import {
-    Flex,
-    Box,
-    FormControl,
-    FormLabel,
-    Input,
-    InputGroup,
-    HStack,
-    InputRightElement,
-    Stack,
-    Button,
-    Heading,
-    Text,
-    useColorModeValue,
-    Link,
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
@@ -24,122 +23,121 @@ import { useNavigate } from "react-router-dom";
 export default function SignupCard() {
     const navigate = useNavigate();
 
-    const [showPassword, setShowPassword] = useState(false);
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  // const [loginResponse, setLoginResponse] = useState(null);
 
-    // const [loginResponse, setLoginResponse] = useState(null);
-
-    // const handleSignUp = async () => {
-    //   // const response = await ApiService.login(email, password);
-    //   // setLoginResponse(response);
-    //   console.log(`Email: ${email}, Password: ${password}, Confirm Password: ${confirmPassword}`);
-    // }
-
+  // const handleSignUp = async () => {
+  //   // const response = await ApiService.login(email, password);
+  //   // setLoginResponse(response);
+  //   console.log(`Email: ${email}, Password: ${password}, Confirm Password: ${confirmPassword}`);
+  // }
+  
 
 
-    return (
-        <Box>
-            <LoginNavbar />
-            <Flex
-                //   minH={"100vh"}
-                align={"center"}
-                justify={"center"}
-                bg={useColorModeValue("gray.50", "gray.800")}
-            >
-                <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-                    <Stack align={"center"}>
-                        <Heading fontSize={"4xl"} textAlign={"center"}>
-                            Sign up
-                        </Heading>
-                        <Text fontSize={"lg"} color={"gray.600"}>
-                            Create an account to access the functionality
-                        </Text>
-                    </Stack>
-                    <Box
-                        rounded={"lg"}
-                        bg={useColorModeValue("white", "gray.700")}
-                        boxShadow={"lg"}
-                        p={8}
-                    >
-                        <Stack spacing={4}>
-                            <HStack>
-                                <Box>
-                                    <FormControl id="firstName" isRequired>
-                                        <FormLabel>First Name</FormLabel>
-                                        <Input type="text" />
-                                    </FormControl>
-                                </Box>
-                                <Box>
-                                    <FormControl id="lastName">
-                                        <FormLabel>Last Name</FormLabel>
-                                        <Input type="text" />
-                                    </FormControl>
-                                </Box>
-                            </HStack>
-                            <FormControl id="email" isRequired>
-                                <FormLabel>Email address</FormLabel>
-                                <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                            </FormControl>
-                            <FormControl id="password" isRequired>
-                                <FormLabel>Password</FormLabel>
-                                <InputGroup>
-                                    <Input type={showPassword ? "text" : "password"} value={password} onChange={(e) => setPassword(e.target.value)} />
-                                    <InputRightElement h={"full"}>
-                                        <Button
-                                            variant={"ghost"}
-                                            onClick={() =>
-                                                setShowPassword((showPassword) => !showPassword)
-                                            }
-                                        >
-                                            {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                                        </Button>
-                                    </InputRightElement>
-                                </InputGroup>
-                            </FormControl>
-                            <FormControl id="confirmPassword" isRequired>
-                                <FormLabel>Confirm password</FormLabel>
-                                <InputGroup>
-                                    <Input type={showConfirmPassword ? "text" : "password"} value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-                                    <InputRightElement h={"full"}>
-                                        <Button
-                                            variant={"ghost"}
-                                            onClick={() =>
-                                                setShowConfirmPassword(
-                                                    (showConfirmPassword) => !showConfirmPassword
-                                                )
-                                            }
-                                        >
-                                            {showConfirmPassword ? <ViewIcon /> : <ViewOffIcon />}
-                                        </Button>
-                                    </InputRightElement>
-                                </InputGroup>
-                            </FormControl>
-                            <Stack spacing={10} pt={2}>
-                                <Button
-                                    loadingText="Submitting"
-                                    size="lg"
-                                    bg={"pink.400"}
-                                    color={"white"}
-                                    _hover={{
-                                        bg: "pink.500",
-                                    }}
-                                >
-                                    Sign up
-                                </Button>
-                            </Stack>
-                            <Stack pt={6}>
-                                <Text align={"center"}>
-                                    Already a user? <Link color={"pink.400"} onClick={() => navigate('/sign-in')}>Sign in</Link>
-                                </Text>
-                            </Stack>
-                        </Stack>
-                    </Box>
-                </Stack>
-            </Flex>
+  return (
+    <Box>
+        <LoginNavbar />
+        <Flex
+    //   minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
+            Sign up
+          </Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            Create an account to access the functionality
+          </Text>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            {/* <HStack>
+              <Box>
+                <FormControl id="firstName" isRequired>
+                  <FormLabel>First Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="lastName">
+                  <FormLabel>Last Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+            </HStack> */}
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input type={showPassword ? "text" : "password"} value={password} onChange={(e) => setPassword(e.target.value)}  />
+                <InputRightElement h={"full"}>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }
+                  >
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <FormControl id="confirmPassword" isRequired>
+              <FormLabel>Confirm password</FormLabel>
+              <InputGroup>
+                <Input type={showConfirmPassword ? "text" : "password"} value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                <InputRightElement h={"full"}>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() =>
+                      setShowConfirmPassword(
+                        (showConfirmPassword) => !showConfirmPassword
+                      )
+                    }
+                  >
+                    {showConfirmPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={"teal.400"}
+                color={"white"}
+                _hover={{
+                  bg: "teal.500",
+                }}
+              >
+                Sign up
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Text align={"center"}>
+                Already a user? <Link color={"teal.400"} onClick={() => navigate('/sign-in')}>Sign in</Link>
+              </Text>
+            </Stack>
+          </Stack>
         </Box>
-    );
+      </Stack>
+    </Flex>
+    </Box>
+  );
 }
