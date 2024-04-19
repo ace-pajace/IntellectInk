6d5a3934f3ed039e84b7d687cffac1eee2536494
matimarecki <mati.marecki@gmail.com>
2024-04-19 15:46:31 +0200

    Feat: Course Copying

--- backend/backend/courses/views.py
+++ backend/backend/courses/views.py
@@ -249,3 +249,60 @@ def create_directory(request):
     parent_directory = Directories.objects.get(directory_id=request.data.get('directory_id'))
     directory = Directories(parent_directory=parent_directory, course=course, name=request.data.get('name'))
     directory.save()
+
+
+def copy_course(request):
+    """
+    Copies specified course, that is all directories will be the same.
+    :param request:
+    Data must contain 'name', 'term', 'edition', the whole shabang used for identifying courses.
+    Data must contain 'nc_name', 'nc_term', 'nc_edition', the details for the newly created course.
+    :return:
+    JSON response with either status 200 - OK or 403 - Forbidden
+    """
+    # if not request.user.is_authenticated:
+    #     return JsonResponse({'error': 'Authentication needed'}, status=401)
+    # user_email = request.user.email
+    mapping = dict()
+    user_email = requests.GET.get('email', request.user.email)
+    user = User.objects.get(email=user_email)
+    course = Courses.objects.get(name=request.POST.get('name'), term=request.POST.get('term'), edition=request.POST.get('edition'))
+    access = CourseAccess.objects.filter(user__email=user_email).filter(course=course)
+    if not access:
+        return JsonResponse({'message': "Course not found or no access to course"}, status=403)
+    if access[0].access_level < 1:
+        return JsonResponse({'message': 'Access denied'}, status=403)
+
+    prev_directories = Directories.objects.filter(course=course)
+    prev_directories_data = [{'id': prevdir.directory_id,
+                              'name': prevdir.name,
+                              'parent': prevdir.parent_directory}
+                             for prevdir in prev_directories]
+    prev_root_dir = prev_directories.get(parent_directory=None)
+    for i in range(len(prev_directories_data)):
+        if prev_directories_data[i]['id'] == prev_root_dir.id:
+            del prev_directories_data[i]
+            break
+
+    mapping = dict()
+
+    course = Courses(term=request.POST.get('nc_term'), name=request.POST.get('nc_name'),
+                     edition=request.POST.get('nc_edition'))
+    course.save()
+    course_access = CourseAccess(user=user, course=course, access_level=3)
+    course_access.save()
+    root_dir = Directories(parent_directory=None, name="root", course=course)
+    root_dir.save()
+    mapping[prev_root_dir.directory_id] = root_dir.directory_id
+
+    while len(prev_directories_data) > 0:
+        for i in range(len(prev_directories_data)):
+            if prev_directories_data[i]['parent'] in list(mapping.keys()):
+                new_dir = Directories(parent_directory=mapping[prev_directories_data[i]['parent']],
+                                      name=prev_directories_data[i]['name'],
+                                      course=course)
+                mapping[prev_directories_data[i]['id']] = new_dir.directory_id
+                del prev_directories_data[i]
+                break
+
+    return JSONResponse({'message': "Successfully copied. Have fun"}, status=200)
